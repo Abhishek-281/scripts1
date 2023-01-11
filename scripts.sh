cdaa61503ea470f3ed2ba7219d4b30e283eb8dc7
Ubuntu <gitvm@gitprac.l1pl33cbukcedk1n0dq5rzgsfd.jnbx.internal.cloudapp.net>
2023-01-11 07:21:32 +0000

    script1 commited

---
+++ scripts.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+name=Abhishek
+age=25
+host=$(hostname)
+pwd=$(pwd)
+
+echo My name is $name  and my age is $age
+echo     My host name $host and my present working directory is $pwd 
+
+echo What is your name?
+read name
+echo welcome to neosoft $name
+
+echo what is your age
+read age
+
+if [ $age -ge 18 ]
+then 
+        echo You can be neosoft Employee!
+else 
+        echo You can be neosoft Intern!
+fi
+
+echo Confirm your age
+
+read age
+
+if [ $age -ge 18 ]
+then 
+        echo What is your employee id ?
+        read id 
+        echo Your $id is Saved at Neosoft database!
+
+else
+        echo You will get temporary id
+
+if
